1475eac1735b07f99a3ce0ab4aef7c8c57bda64d
stephepush <7speter@gmail.com>
2020-07-29 11:03:41 -0400

    Adds prelim seeding, model associations

--- app/models/car_item.rb
+++ app/models/car_item.rb
@@ -1,2 +1,15 @@
 class CarItem < ApplicationRecord
+    belongs_to :ModelInfo
+
+    def ModelName
+        self.ModelInfo.name
+    end
+
+    def ModelMake
+        self.ModelInfo.model
+    end
+
+    def CarModel
+        self.ModelInfo.make
+    end
 end
